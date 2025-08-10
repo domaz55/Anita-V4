ed817f0e3a2c08fbc3798c1ccd2975031ca6bbcd
domaz55 <stdominicshighm@gmail.com>
2025-08-10 16:38:45 +0200

    config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 const toBool = (value) => value === "true";
 
 // Owner & Bot Details
-global.owner = process.env.OWNER_NUMBER;
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.owner = process.env.OWNER_NUMBER;"263785439377"
+global.anitav4 = process.env.BOT_NAME || "PRINCE_MD";
+global.ownername = process.env.OWNER_NAME || "PRINCE";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.author = process.env.AUTHOR || "PRINCE";
+global.channelname = process.env.CHANNEL_NAME || "PRINCE";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 
 // Auto-Features
